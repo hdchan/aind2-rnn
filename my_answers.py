da365385316a1d653ff4c1a52a8a8b2060174306
Henry Chan <henry.chan@tigerspike.com>
2018-04-26 00:09:50 +1000

    Answer 2.4

--- my_answers.py
+++ my_answers.py
@@ -55,9 +55,10 @@ def cleaned_text(text):
 
 ### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_text(text, window_size, step_size):
+    # print(text[:100], window_size, step_size)
     # containers for input/output pairs
-    inputs = []
-    outputs = []
+    inputs = [text[x : x + window_size] for x in range(0, len(text) - window_size, step_size)]
+    outputs = [text[x] for x in range(window_size, len(text), step_size) ]
 
     return inputs,outputs
 
